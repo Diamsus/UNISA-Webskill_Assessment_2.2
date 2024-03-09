bd81e9ce83a873abadf8dd7198efbf8b691e1616
Diamsus <tjm1987@gmail.com>
2024-03-09 20:59:28 +1000

    ### Added images to a seperate folder and adjusted links accordingly
    - Moved all images to their own folder for organisation
    -fixed the links for all html pages

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-    // creating variables for best selling artworks
-    const saleItemOne = {image:"boat.jpeg",price:"$122.95 AUD"}
-    const saleItemTwo = {image:"woodmap.jpg", price:"$24.95 AUD"}
+    // creating variables for best selling artworks added sources both locally and via URL to display different usages
+    const saleItemOne = {image:"Images/boat.jpeg",price:"$122.95 AUD"}
+    const saleItemTwo = {image:"Images/woodmap.jpg", price:"$24.95 AUD"}
     const saleItemThree = {image:"https://images.pexels.com/photos/11105145/pexels-photo-11105145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", price:"$82.95 AUD"}
     const saleItemFour = {image:"https://images.pexels.com/photos/12727634/pexels-photo-12727634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", price:"$60.00 AUD"}
     const saleItemFive = {image:"https://images.pexels.com/photos/4609253/pexels-photo-4609253.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", price:"$399.00 AUD"}
